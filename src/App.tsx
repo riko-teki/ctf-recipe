38bda66cb4eb0e3c1515ddb4b30100d86d1a8716
rikoteki <boiledshrimps01092gmail.com>
2021-10-04 16:39:30 +0900

    add: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./components/pages/home";
+import NotFound from "./components/pages/404";
+import Header from "./components/containers/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <div className={classes.container}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </>
   );
 }
 
